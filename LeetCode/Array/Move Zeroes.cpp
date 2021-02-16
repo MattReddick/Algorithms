a72761fb9fec3bd0ade2dfeacb050132ec9fd48e
MattReddick <matthewreddick@ucsb.edu>
2021-02-16 15:27:13 -0800

    Create Move Zeroes.cpp

---
+++ LeetCode/Array/Move Zeroes.cpp
@@ -0,0 +1,30 @@
+/*
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+
+*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int counter = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0){
+                nums.erase(nums.begin()+i);
+                i--;
+                counter++;
+            }
+        }
+        for(int i = 0; i < counter; i++){
+            nums.push_back(0);
+        }
+    }
+};
