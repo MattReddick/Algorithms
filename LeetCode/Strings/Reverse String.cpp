977ce8b6b53045750a390524a13a6f90d37a65b5
MattReddick <matthewreddick@ucsb.edu>
2021-04-27 00:42:12 -0700

    Update Reverse String.cpp

--- LeetCode/Strings/Reverse String.cpp
+++ LeetCode/Strings/Reverse String.cpp
@@ -1,1 +1,9 @@
-
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for(int i = 0; i < s.size()/2;i++){
+            swap(s[s.size()-i-1],s[i]);
+           
+        }
+    }
+};
